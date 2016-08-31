232d081c0de8e4eb321064277c649150a03c4b52
Tomas Ryba <rybato@gmail.com>
2016-08-31 06:37:26 +0200

    drobnosti

--- lession_localization.py
+++ lession_localization.py
@@ -3,6 +3,9 @@ from __future__ import division
 import os
 import sys
 
+import cPickle as pickle
+import gzip
+
 import matplotlib.pyplot as plt
 import numpy as np
 from skimage import img_as_float
@@ -28,7 +31,7 @@ else:
     sys.exit(0)
 
 
-def calc_saliencies(data, mask, smoothing=True, show=False, show_now=True):
+def calc_saliencies(data, mask, smoothing=True, save=False, types='dhgstcb', out_fname='out.pklz', show=False, show_now=True):
     use_sigmoid = True
     morph_proc = True
 
@@ -41,28 +44,40 @@ def calc_saliencies(data, mask, smoothing=True, show=False, show_now=True):
     data_struc = [(data, 'input'),]
     data = img_as_float(data)
     print 'calculating:',
-    print 'int diff ...',
-    id = sf.conspicuity_calculation(data, sal_type='int_diff', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
-    data_struc.append((id, 'int diff'))
-    print 'int hist ...',
-    ih = sf.conspicuity_calculation(data, sal_type='int_hist', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
-    data_struc.append((ih, 'int hist'))
-    print 'int glcm ...',
-    ig = sf.conspicuity_calculation(data, sal_type='int_glcm', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
-    data_struc.append((ig, 'int glcm'))
-    print 'int sliwin ...',
-    isw = sf.conspicuity_calculation(data, sal_type='int_sliwin', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
-    data_struc.append((isw, 'int sliwin'))
-    print 'texture ...',
-    t = sf.conspicuity_calculation(data, sal_type='texture', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=False)
-    data_struc.append((t, 'texture'))
-    print 'circloids ...',
-    circ = sf.conspicuity_calculation(data, sal_type='circloids', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=True)
-    data_struc.append((circ, 'circloids'))
-    print 'blobs ...',
-    blobs = sf.conspicuity_calculation(data, sal_type='blobs', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=True, verbose=False)
-    data_struc.append((blobs, 'blobs'))
+    if 'd' in types:
+        print 'int diff ...',
+        id = sf.conspicuity_calculation(data, sal_type='int_diff', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
+        data_struc.append((id, 'int diff'))
+    if 'h' in types:
+        print 'int hist ...',
+        ih = sf.conspicuity_calculation(data, sal_type='int_hist', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
+        data_struc.append((ih, 'int hist'))
+    if 'g' in types:
+        print 'int glcm ...',
+        ig = sf.conspicuity_calculation(data, sal_type='int_glcm', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
+        data_struc.append((ig, 'int glcm'))
+    if 's' in types:
+        print 'int sliwin ...',
+        isw = sf.conspicuity_calculation(data, sal_type='int_sliwin', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=morph_proc)
+        data_struc.append((isw, 'int sliwin'))
+    if 't' in types:
+        print 'texture ...',
+        t = sf.conspicuity_calculation(data, sal_type='texture', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=False)
+        data_struc.append((t, 'texture'))
+    if 'c' in types:
+        print 'circloids ...',
+        circ = sf.conspicuity_calculation(data, sal_type='circloids', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=True)
+        data_struc.append((circ, 'circloids'))
+    if 'b' in types:
+        print 'blobs ...',
+        blobs = sf.conspicuity_calculation(data, sal_type='blobs', mask=mask, calc_features=False, use_sigmoid=False, morph_proc=True, verbose=False)
+        data_struc.append((blobs, 'blobs'))
     print 'done'
+
+    if save:
+        f = gzip.open(out_fname, 'wb', compresslevel=1)
+        pickle.dump(data_struc, f)
+        f.close()
 
     if show:
         plt.figure()
@@ -89,21 +104,33 @@ def localize(data, mask, show=False, show_now=True):
 def get_data_struc():
     '''
     datastruc: [(venous data, arterial data, slices, offset), ...]
+    datastruc: [(venous data, arterial data, slices), ...]
     '''
     dirpath = '/home/tomas/Dropbox/Data/medical/dataset/gt/'
     s = 4
-    data_struc = [('180_venous-GT.pklz', '180_arterial-GT.pklz', range(6,14), 0),
-                  ('183a_venous-GT.pklz', '183a_arterial-GT.pklz', range(11, 24, s), -1),
-                  ('185a_venous-GT.pklz', '185a_arterial-GT.pklz', range(9, 24, s), 1),
-                  ('186a_venous-GT.pklz', '186a_arterial-GT.pklz', range(1, 5, s), 0),
-                  ('189a_venous-GT.pklz', '189a_arterial-GT.pklz', range(12, 26, s), -1),
-                  ('221_venous-GT.pklz', '221_arterial-GT.pklz', range(17, 23, s), 0),
-                  ('222a_venous-GT.pklz', '222a_arterial-GT.pklz', range(0, 0, s), -2),
-                  ('232_venous-GT.pklz', '232_arterial-GT.pklz', range(0, 0, s), -2),
-                  ('234_venous-GT.pklz', '234_arterial-GT.pklz', range(0, 0, s), 0),
-                  ('235_venous-GT.pklz', '235_arterial-GT.pklz', range(0, 0, s), 0)]
+    # data_struc = [('180_venous-GT.pklz', '180_arterial-GT.pklz', range(6,14), 0),
+    #               ('183a_venous-GT.pklz', '183a_arterial-GT.pklz', range(11, 24, s), -1),
+    #               ('185a_venous-GT.pklz', '185a_arterial-GT.pklz', range(9, 24, s), 1),
+    #               ('186a_venous-GT.pklz', '186a_arterial-GT.pklz', range(1, 5, s), 0),
+    #               ('189a_venous-GT.pklz', '189a_arterial-GT.pklz', range(12, 26, s), -1),
+    #               ('221_venous-GT.pklz', '221_arterial-GT.pklz', range(17, 23, s), 0),
+    #               ('222a_venous-GT.pklz', '222a_arterial-GT.pklz', range(0, 0, s), -2),
+    #               ('232_venous-GT.pklz', '232_arterial-GT.pklz', range(0, 0, s), -2),
+    #               ('234_venous-GT.pklz', '234_arterial-GT.pklz', range(0, 0, s), 0),
+    #               ('235_venous-GT.pklz', '235_arterial-GT.pklz', range(0, 0, s), 0)]
+    data_struc = [('180_venous-GT.pklz', '180_arterial-GT.pklz', range(6, 14)),
+                  ('183a_venous-GT.pklz', '183a_arterial-GT.pklz', range(11, 23, s)),
+                  ('185a_venous-GT.pklz', '185a_arterial-GT.pklz', range(9, 24, s)),
+                  ('186a_venous-GT.pklz', '186a_arterial-GT.pklz', range(1, 7, s)),
+                  ('189a_venous-GT.pklz', '189a_arterial-GT.pklz', range(12, 26, s)),
+                  ('221_venous-GT.pklz', '221_arterial-GT.pklz', range(17, 23, s)),
+                  ('222a_venous-GT.pklz', '222a_arterial-GT.pklz', range(1, 9, s) + range(23, 30, s)),
+                  ('232_venous-GT.pklz', '232_arterial-GT.pklz', range(1, 12, s) + range(17, 25, s)),
+                  ('234_venous-GT.pklz', '234_arterial-GT.pklz', range(0, 0, s)),
+                  ('235_venous-GT.pklz', '235_arterial-GT.pklz', range(11, 42, s))]
 
-    data_struc = [(dirpath + x[0], dirpath + x[1], x[2], x[3]) for x in data_struc]
+    # data_struc = [(dirpath + x[0], dirpath + x[1], x[2], x[3]) for x in data_struc]
+    data_struc = [(dirpath + x[0], dirpath + x[1], x[2]) for x in data_struc]
 
     return data_struc
 
@@ -111,33 +138,45 @@ def get_data_struc():
 ################################################################################
 ################################################################################
 if __name__ == '__main__':
+    # TODO 1: seshapovat data -  ulozit
+    # TODO 2: registrovat po rezech - ulozit
+    # TODO 3: spocitat salmapy - ulozit
+    # TODO 4: MRF experimenty
+    # TODO 5: vybrat idealni kombinaci salmap
+    # TODO 6: porovnat data
+
     # zoomovani tak aby mela data stejny pocet rezu
-    d1, m1, vs1 = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_venous-GT.pklz')
-    d2, m2, vs2 = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_arterial-GT.pklz')
-    d3 = tools.match_size(d2, d1.shape)
-    m3 = tools.match_size(m2, m1.shape)
-
-    datap = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_arterial-GT.pklz', return_datap=True)
-
-    slab = {'none': 0, 'liver': 1, 'lesions': 6}
-    datap_1 = {'data3d': d1, 'segmentation': m1, 'voxelsize_mm': vs1, 'slab': slab}
-    datap_2 = {'data3d': d3, 'segmentation': m3, 'voxelsize_mm': vs2, 'slab': slab}
-    app = QtGui.QApplication(sys.argv)
-    le = SegViewer(datap1=datap_1, datap2=datap_2)
-    le.show()
-    app.exec_()
+    # d1, m1, vs1 = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_venous-GT.pklz')
+    # d2, m2, vs2 = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_arterial-GT.pklz')
+    # d3 = tools.match_size(d2, d1.shape)
+    # m3 = tools.match_size(m2, m1.shape)
+    #
+    # # datap = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/222a_arterial-GT.pklz', return_datap=True)
+    #
+    # slab = {'none': 0, 'liver': 1, 'lesions': 6}
+    # datap_1 = {'data3d': d1, 'segmentation': m1, 'voxelsize_mm': vs1, 'slab': slab}
+    # datap_2 = {'data3d': d3, 'segmentation': m3, 'voxelsize_mm': vs2, 'slab': slab}
+    # app = QtGui.QApplication(sys.argv)
+    # le = SegViewer(datap1=datap_1, datap2=datap_2)
+    # le.show()
+    # app.exec_()
 
     # vizualizace dat
-    # ds = get_data_struc()
-    #
-    # # for i, (vfn, afn, slcs, off) in enumerate(ds):
-    # i = 6
-    # vfn, afn, slcs, off = ds[i]
+    ds = get_data_struc()
+
+    # for i, (vfn, afn, slcs, off) in enumerate(ds):
+    # i = 8
+    # vfn, afn, slcs = ds[i]
     #
     # print i, ' ... ', vfn.split('/')[-1], afn.split('/')[-1]
     #
     # datap_1 = tools.load_pickle_data(vfn, return_datap=True)
-    # datap_2 = tools.load_pickle_data(afn, return_datap=True)
+    # # datap_2 = tools.load_pickle_data(afn, return_datap=True)
+    # d2, m2, vs2 = tools.load_pickle_data(afn)
+    # d2 = tools.match_size(d2, datap_1['data3d'].shape)
+    # m2 = tools.match_size(m2, datap_1['segmentation'].shape)
+    # slab = {'none': 0, 'liver': 1, 'lesions': 6}
+    # datap_2 = {'data3d': d2, 'segmentation': m2, 'voxelsize_mm': vs2, 'slab': slab}
     #
     # app = QtGui.QApplication(sys.argv)
     # le = SegViewer(datap1=datap_1, datap2=datap_2)
@@ -158,6 +197,12 @@ if __name__ == '__main__':
     #     data, gt_mask, voxel_size = tools.load_pickle_data(f)
     #     data = tools.windowing(data)
     #     tools.show_3d(data, range=False)
+
+    # nejaka vizualizace
+    # data, gt_mask, voxel_size = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/234_arterial-.pklz')
+    # data, gt_mask, voxel_gtsize = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/234_venous-GT.pklz')
+    # data = tools.windowing(data)
+    # tools.show_3d(gt_mask, range=False)
 
     # data, mask, _ = tools.load_pickle_data('/home/tomas/Dropbox/Data/medical/dataset/gt/183a_venous-GT.pklz')
     # slice_ind = 17
